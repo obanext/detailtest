f223d1b4b3612592654bbeb79f78375a4bdef1dd
obanext <93115438+obanext@users.noreply.github.com>
2026-04-20 12:30:38 +0200

    Refactor wise.js to add API handler function

---
+++ pages/api/wise.js
@@ -0,0 +1,23 @@
+export default async function handler(req, res) {
+  const { id } = req.query;
+
+  const base = "https://bibliotheek-accept1.wise.oclc.org/restapi";
+
+  const headers = {
+    Accept: "application/json",
+    application: process.env.APPLICATION,
+    WISE_KEY: process.env.WISE_KEY
+  };
+
+  try {
+    const [title, availability, summary] = await Promise.all([
+      fetch(`${base}/discovery/title/${id}`, { headers }).then(r => r.json()),
+      fetch(`${base}/branch/1000/titleavailability/${id}?clientType=PUBLIC`, { headers }).then(r => r.json()),
+      fetch(`${base}/discovery/titlesummary/${id}?addRelatedTitles=true&includeManifestations=true`, { headers }).then(r => r.json())
+    ]);
+
+    res.status(200).json({ title, availability, summary });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+}
