b8b38025bf0a5223bc6afbbfc14467bab4d0a59d
obanext <93115438+obanext@users.noreply.github.com>
2026-04-21 05:28:34 +0200

    Refactor item information fetch to remove branchId

--- pages/api/wise.js
+++ pages/api/wise.js
@@ -19,9 +19,7 @@ export default async function handler(req, res) {
         `${base}/discovery/titlesummary/${id}?addRelatedTitles=true&includeManifestations=true`,
         { headers }
       ).then((r) => r.json()),
-      fetch(`${base}/title/${id}/iteminformation?branchId=1000&clientType=PUBLIC`, {
-        headers,
-      }).then((r) => r.json()),
+      fetch(`${base}/title/${id}/iteminformation`, { headers }).then((r) => r.json()),
     ]);
 
     res.status(200).json({ title, availability, summary, itemInformation });
