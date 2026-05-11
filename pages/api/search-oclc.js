f235e04131fa5b14e0ed4a286dc23207d1b01aa3
obanext <93115438+obanext@users.noreply.github.com>
2026-05-11 15:54:12 +0200

    Update WISE_BASE_URL to point to accept1 environment

--- pages/api/search-oclc.js
+++ pages/api/search-oclc.js
@@ -1,4 +1,4 @@
-const BASE = process.env.WISE_BASE_URL || "https://bibliotheek-accept8.wise.oclc.org/restapi";
+const BASE = process.env.WISE_BASE_URL || "https://bibliotheek-accept1.wise.oclc.org/restapi";
 const BRANCH_ID = process.env.WISE_BRANCH_ID || "1000";
 const CLIENT_TYPE = process.env.WISE_CLIENT_TYPE || "default";
 const DEFAULT_PERSPECTIVE_ID = process.env.WISE_DEFAULT_PERSPECTIVE_ID || "3687";
